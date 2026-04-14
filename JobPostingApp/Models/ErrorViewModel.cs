01ad2469bc77a9c745e197903b6bda6ae0f7c2c7
joseninogarcia06 <jngarcia08081988@gmail.com>
2026-04-14 15:13:06 +0800

    Initial Commit

---
+++ JobPostingApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace JobPostingApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
